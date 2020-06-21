570f1976bcde92dfb8c9f812e31561c36fab7b3b
Foreman <sprain@yandex.ru>
2020-06-21 15:05:35 +0300

    Задание № 1-2 урока № 4 - для mail.ru + lenta.ru + yandex.ru

--- 4 урок/les4_task1,2.py
+++ 4 урок/les4_task1,2.py
@@ -2,7 +2,7 @@ import requests
 from lxml import html
 from pymongo import MongoClient
 from pprint import pprint
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import time
 
 news_link_mail = 'https://news.mail.ru'
@@ -19,7 +19,11 @@ news = db.news
 
 
 def pars_news_mail():
+    """Парсинг новостей с https://news.mail.ru"""
+
     def get_all_news_links_on_start_page(page_html: str) -> list:
+        """Получение всех ссылок на новостные статьи со стартовой страницы"""
+
         dom = html.fromstring(page_html)
         blocks = dom.xpath("//a[@class='photo photo_full photo_scale js-topnews__item'] | \
         //a[@class='photo photo_small photo_scale photo_full js-topnews__item'] | \
@@ -33,10 +37,13 @@ def pars_news_mail():
         return links_on_start_page
 
     def get_news_data(news_link: str) -> dict:
+        """Получение требуемых по условию данных о конкретной новостной статье по ссылке"""
+
         news_dict = {}
         response = requests.get(news_link, headers=headers)
         news_dom = html.fromstring(response.text)
-        news_dict['news_headline'] = news_dom.xpath("//h1[@class='hdr__inner']/text()")[0]
+        news_dict['news_headline'] = news_dom.xpath("//h1[@class='hdr__inner']/text()")[0].replace(
+            '\xa0', ' ').replace('&nbsp;', ' ')
         news_dict['source_name'] = news_dom.xpath("//a[@class='link color_gray breadcrumbs__link']/span/text()")[0]
         news_dict['news_date'] = convert_news_date(
             news_dom.xpath("//span[@class='note__text breadcrumbs__text js-ago']/@datetime")[0])
@@ -54,7 +61,11 @@ def pars_news_mail():
 
 
 def pars_news_lenta():
+    """Парсинг новостей с https://lenta.ru"""
+
     def get_all_news_links_on_start_page(page_html: str) -> list:
+        """Получение всех ссылок на новостные статьи со стартовой страницы"""
+
         dom = html.fromstring(page_html)
         blocks = dom.xpath("//div[contains(@class, 'item')]//a[contains(@href, 'news')]")
         links_on_start_page = []
@@ -65,11 +76,13 @@ def pars_news_lenta():
         return links_on_start_page
 
     def get_news_data(news_link: str) -> dict:
+        """Получение требуемых по условию данных о конкретной новостной статье по ссылке"""
+
         news_dict = {}
         response_news_page = requests.get(news_link, headers=headers)
         news_dom = html.fromstring(response_news_page.text)
         news_dict['news_headline'] = news_dom.xpath("//h1[contains(@class, 'b-topic__title')]/text()")[0].replace(
-            '\xa0', ' ')
+            '\xa0', ' ').replace('&nbsp;', ' ')
         news_dict['source_name'] = 'Lenta.RU'
         news_dict['news_date'] = convert_news_date(
             news_dom.xpath("//div[contains(@class, 'b-topic__info')]//@datetime")[0])
@@ -87,15 +100,37 @@ def pars_news_lenta():
 
 
 def pars_news_yandex():
-    pass
+    """Парсинг новостей с https://yandex.ru/news"""
+
+    response = requests.get(news_link_yandex, headers=headers)
+    dom = html.fromstring(response.text)
+    blocks = dom.xpath(
+        "//div[@class='story__content'] | //td[@class='stories-set__item']")
+    news_dict = {}
+    for block in blocks:
+        news_dict['news_headline'] = block.xpath(".//h2[@class='story__title']//text()")[0]
+        story_info = block.xpath(".//div[@class='story__date']/text()")[0].replace('\xa0', ' ').replace('&nbsp;',
+                                                                                                        ' ').split(' ')
+        if len(story_info) >= 4 and story_info[-3] == 'вчера':
+            news_dict['source_name'] = ' '.join(story_info[:-3])
+            news_dict['news_date'] = (date.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+        else:
+            news_dict['source_name'] = ' '.join(story_info[:-1])
+            news_dict['news_date'] = date.today().strftime('%d.%m.%Y')
+        news_dict['news_link'] = 'https://yandex.ru' + block.xpath(".//h2[@class='story__title']//@href")[0]
+        news_dict['updated_date'] = date.today().strftime('%d.%m.%Y')
+        news.update_one({'news_link': news_dict['news_link']}, {'$set': news_dict}, upsert=True)
+        pprint(news_dict)
 
 
 def convert_news_date(date: str) -> str:
+    """Конвертация даты для единообразия"""
+
     return datetime.strptime(date.split('+')[0], '%Y-%m-%dT%H:%M:%S').strftime('%d.%m.%Y')
 
 
 def delete_old_news():
-    """Перед окончанием работы программы, проверка и удаление закрытых на сайте вакансий"""
+    """Перед окончанием работы программы, проверка и удаление записей в БД об устаревших новостных статьях"""
 
     result = news.delete_many({'updated_date': {'$ne': date.today().strftime('%d.%m.%Y')}})
     print(f'Удалено {result.deleted_count} устаревших записей.')
