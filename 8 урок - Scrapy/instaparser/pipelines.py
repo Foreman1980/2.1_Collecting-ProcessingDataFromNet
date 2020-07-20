c2c89edc4e66cd738a49085879acdc3689fc6aca
Foreman <sprain@yandex.ru>
2020-07-20 21:07:17 +0300

    Задания № 1-3 урока № 8. Выполненно.

--- 8 урок - Scrapy/instaparser/pipelines.py
+++ 8 урок - Scrapy/instaparser/pipelines.py
@@ -6,9 +6,28 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 
 class InstaparserPipeline:
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client['instagram']
+
     def process_item(self, item, spider):
-        pass
+        collection = self.mongo_base['users']
+
+        # На случай взаимной подписки (c исследуемым пользователем), чтобы не задублировать пользователя, а только
+        # обновить списки подписок (для примера см. 'noxlis')
+        result = collection.find_one({'user_id': item['user_id']})
+        if result:
+            if result['followers']:
+                followers_list = item['followers']
+                followers_list.extend(result['followers'])
+                item['followers'] = list(set(followers_list))
+            elif result['following']:
+                following_list = item['following']
+                following_list.extend(result['following'])
+                item['following'] = list(set(following_list))
+        collection.update_one({'user_id': item['user_id']}, {'$set': item}, upsert=True)
         return item
--- 8 урок - Scrapy/instaparser/spiders/instagram.py
+++ 8 урок - Scrapy/instaparser/spiders/instagram.py
@@ -14,7 +14,8 @@ class InstagramSpider(scrapy.Spider):
     insta_login_link = 'https://www.instagram.com/accounts/login/ajax/'
     insta_login = 'piotrivanov850'
     insta_pwd = '#PWD_INSTAGRAM_BROWSER:10:1595170029:AdVQAF+XIYgEcNyPUv9HKc5IxJEJujit7JkranmhB9KmhPMPOltaqovLpNVFfrqaYpZxCke6realywZGT6LMG74Evk5AUvRcBQyHIG1eidnrVpbntW+ZNS54hO1tGbiEy8nq4MSVLp9HzZUd'
-    parse_user = 'kumzhev'
+    # parse_user = 'kumzhev'
+    parse_user = 'p.shkolyanskiy'
 
     graphql_url = 'https://www.instagram.com/graphql/query/?'
     post_hash = '15bf78a4ad24e33cbd838fdb31353ac1'
@@ -41,78 +42,82 @@ class InstagramSpider(scrapy.Spider):
             )
 
     def user_data_parse(self, response: HtmlResponse, user_name):
-        full_name = response.xpath('//h1/text()').extract_first()
-        profil_pic_url = response.xpath('//main//img[@data-testid="user-avatar"]/@src').extract_first()
         user_id = self.fetch_user_id(response.text, user_name)
-        # 1 '{"id":"8995164690","include_reel":true,"fetch_mutual":true,"first":24}'
+
+        # Парсим список подписчиков
         var_followers = {'id': user_id,
                          'include_reel': True,
-                         #'fetch_mutual': True,
+                         # 'fetch_mutual': True, # всё работает и без этого параметра
                          'first': 24,
                          }
         url_followers = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(var_followers)}'
         yield response.follow(
             url_followers,
             callback=self.user_followers_parse,
-            cb_kwargs={'user_name': user_name,
-                       'full_name': full_name if full_name else 'No data',
-                       'user_id': user_id,
-                       'profil_pic_url': profil_pic_url,
-                       'variables': deepcopy(var_followers),
-                       }
+            cb_kwargs={'user_name': user_name, 'var_followers': deepcopy(var_followers)}
         )
 
-        # # 1 '{"id":"8995164690","include_reel":true,"fetch_mutual":false,"first":24}'
-        # var_followings = {'id': user_id,
-        #                   'include_reel': True,
-        #                   'fetch_mutual': False,
-        #                   'first': 24,
-        #                   }
-        # # 2 '{"id":"8995164690","include_reel":true,"fetch_mutual":false,"first":12,"after":"QVFBLXFDZ0RTSkVEdG1uMVQ3LTNtMkRNMHM2Z1h5Qks0UlpOZmwzeDRVWHJUa3dHZ2tPQkdLTm5NcG8wcWp2OG81T3o3REFWVTNYZzg1R0F3VklFZ2RIRw=="}'
-        # url_followers = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(var_followers)}'
-        # yield response.follow(
-        #     url_followers,
-        #     callback=self.user_followers_parse,
-        #     cb_kwargs={'username': username,
-        #                'user_id': user_id,
-        #                'variables': deepcopy(variables),
-        #                }
-        # )
+        # Парсим список тех, на кого подписан пользователь
+        var_following = {'id': user_id,
+                         'include_reel': True,
+                         # 'fetch_mutual': False, # всё работает и без этого параметра
+                         'first': 24,
+                         }
+        url_following = f'{self.graphql_url}query_hash={self.following_hash}&{urlencode(var_following)}'
+        yield response.follow(
+            url_following,
+            callback=self.user_following_parse,
+            cb_kwargs={'user_name': user_name, 'var_following': deepcopy(var_following)}
+        )
 
-    def user_followers_parse(self, response: HtmlResponse, user_name, full_name, user_id, profil_pic_url, variables):
+    def user_followers_parse(self, response: HtmlResponse, user_name, var_followers):
         j_data = json.loads(response.text)
         if j_data['status'] == 'ok':
             page_info = j_data.get('data').get('user').get('edge_followed_by').get('page_info')
             if page_info.get('has_next_page'):
-                variables['after'] = page_info.get('end_cursor')
-                url_followers = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(variables)}'
+                var_followers['after'] = page_info.get('end_cursor')
+                var_followers['first'] = 12
+                url_followers = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(var_followers)}'
                 yield response.follow(
                     url_followers,
                     callback=self.user_followers_parse,
-                    cb_kwargs={'user_name': user_name,
-                               'full_name': full_name,
-                               'user_id': user_id,
-                               'profil_pic_url': profil_pic_url,
-                               'variables': deepcopy(variables),
-                               }
+                    cb_kwargs={'user_name': user_name, 'var_followers': deepcopy(var_followers)}
                 )
         followers = j_data.get('data').get('user').get('edge_followed_by').get('edges')
-        followers_list = []
-        for follower in followers:
-            # followers_list += follower.get('node').get('username')
+        for user in followers:
             yield InstaparserItem(
-                user_name=follower.get('node').get('username'),
-                full_name=follower.get('node').get('full_name'),
-                user_id=follower.get('node').get('id'),
-                profil_pic_url=follower.get('node').get('profile_pic_url'),
+                user_name=user.get('node').get('username'),
+                full_name=user.get('node').get('full_name'),
+                user_id=user.get('node').get('id'),
+                profil_pic_url=user.get('node').get('profile_pic_url'),
                 following=[user_name],
-                followers=[]
+                followers=[],
             )
 
-    # def user_followings_parse(self, response: HtmlResponse, user_name, user_id, variables):
-    #     j_data = json.loads(response.text)
-    #     pass
-    #
+    def user_following_parse(self, response: HtmlResponse, user_name, var_following):
+        j_data = json.loads(response.text)
+        if j_data['status'] == 'ok':
+            page_info = j_data.get('data').get('user').get('edge_follow').get('page_info')
+            if page_info.get('has_next_page'):
+                var_following['after'] = page_info.get('end_cursor')
+                var_following['first'] = 12
+                url_following = f'{self.graphql_url}query_hash={self.following_hash}&{urlencode(var_following)}'
+                yield response.follow(
+                    url_following,
+                    callback=self.user_following_parse,
+                    cb_kwargs={'user_name': user_name, 'var_following': deepcopy(var_following)}
+                )
+        following = j_data.get('data').get('user').get('edge_follow').get('edges')
+        for user in following:
+            yield InstaparserItem(
+                user_name=user.get('node').get('username'),
+                full_name=user.get('node').get('full_name'),
+                user_id=user.get('node').get('id'),
+                profil_pic_url=user.get('node').get('profile_pic_url'),
+                following=[],
+                followers=[user_name],
+            )
+
     def fetch_csrf_token(self, page_http: str) -> str:
         pattern = re.compile('\"csrf_token\":\"\\w+\"')
         return re.search(pattern, page_http).group().split(':').pop().strip('"')
